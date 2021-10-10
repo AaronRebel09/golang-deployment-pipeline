234ce79d105e8cd0501984af3a4cf78143f4ce41
aron <aron@MacBook-Pro-de-Macbook.local>
2021-10-10 00:59:38 -0500

    after test

--- codedeploy/AfterInstall.sh
+++ codedeploy/AfterInstall.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/server/* app
-chmod +x /home/ec2-user/server/ApplicationStart.sh
-chmod +x /home/ec2-user/server/ApplicationStop.sh
-chmod +x /home/ec2-user/server/ValidateService.sh
-chmod +x /home/ec2-user/server/BeforeInstall.sh
-chmod +x /home/ec2-user/server/AfterInstall.sh
+chmod +x /home/ec2-user/server/*
