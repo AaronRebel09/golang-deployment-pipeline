4f4d870b7d0c40089aa655c0d48627861ff16e79
aron <aron@MacBook-Pro-de-Macbook.local>
2021-10-24 04:32:24 -0500

    temporal change for permit test company

--- controllers/userController.go
+++ controllers/userController.go
@@ -115,7 +115,7 @@ func SignUp() gin.HandlerFunc {
 		user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 		user.ID = primitive.NewObjectID()
 		user.User_id = user.ID.Hex()
-		token, refreshToken, _ := helper.GenerateAllTokens(*user.Email, *user.First_name, *user.Last_name, user.User_id)
+		token, refreshToken, _ := helper.GenerateAllTokens(*user.Email, *user.Company, *user.Nit_company, user.User_id)
 		user.Token = &token
 		user.Refresh_token = &refreshToken
 		resultInsertionNumber, insertErr := userCollection.InsertOne(ctx, user)
@@ -152,8 +152,8 @@ func Login() gin.HandlerFunc {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
 			return
 		}
-		token, refreshToken, _ := helper.GenerateAllTokens(*foundUser.Email, *foundUser.First_name,
-			*foundUser.Last_name, foundUser.User_id)
+		token, refreshToken, _ := helper.GenerateAllTokens(*foundUser.Email, *foundUser.Company,
+			*foundUser.Nit_company, foundUser.User_id)
 		helper.UpdateAllTokens(token, refreshToken, foundUser.User_id)
 		c.JSON(http.StatusOK, foundUser)
 	}
